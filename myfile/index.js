d101ec210f09489db6ae36e6e420b4acbc6ede33
Mayank singh <mayank23378@gmail.com>
2024-03-06 13:04:43 +0530

    done with home

---
+++ myfile/index.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+const icon = document.getElementById('icon');
+
+icon.addEventListener('click', () => {
+
+    console.log('mayank singh');
+    return menu.classList.toggle('hidden');
+});
+
+
+
+
+
