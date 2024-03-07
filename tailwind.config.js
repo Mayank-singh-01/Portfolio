22b2d2be601836607860ead8dd89f326498c293e
Mayank singh <mayank23378@gmail.com>
2024-03-08 00:00:56 +0530

    arranging file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Portfolio/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mycolor: '#24b3a6',
+        mypink: '#cc1039',
+      }
+    },
+  },
+  plugins: [],
+}
+
